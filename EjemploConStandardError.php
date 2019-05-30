3fbf460d177aed86009983d38689fe91cb5c87fd
bryann17 <49592168+bryann17@users.noreply.github.com>
2019-05-30 12:12:20 +0200

    Update EjemploConStandardError.php

--- EjemploConStandardError.php
+++ EjemploConStandardError.php
@@ -5,9 +5,9 @@ $i = 0;
 
 while(true){
   if (++$i % $d == 0){
-    fwrite(STDOUT, sprintf("El %d es multiplo de %d.\n", si $i, $d));
+    fwrite(STDOUT, sprintf("El %d es multiplo de %d.\n", si $i, $d)); #Si esta correcto y es multiplo lo envia al Standar de salida
   } else{
-    fwrite(STDERR, sprintf("Error, El %d NO es multiplo de %d \n", $i, $d));
+    fwrite(STDERR, sprintf("Error, El %d NO es multiplo de %d \n", $i, $d)); #Si presenta fallo lo envia al standar de error
   }
   sleep(1);
  }
